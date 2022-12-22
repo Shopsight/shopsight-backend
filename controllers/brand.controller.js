f4de223c75567df691153128484df490aaaa9e5d
Shubham Mondal <shubhamank002@gmail.com>
2022-12-22 16:05:28 +0530

    Add brand route

--- controllers/brand.controller.js
+++ controllers/brand.controller.js
@@ -19,12 +19,19 @@ const getBrandProducts = async (req, res) => {
     try {
         const brandId = req.params.brandId;
         const getProducts =
-            "SELECT brand.name AS brandName, product.imageLink, product.name, product.id FROM brand RIGHT JOIN product ON brand.id = product.brandId WHERE brand.id = ?";
+            "SELECT brand.name AS brandName, product.imageLink, product.name, product.id, color, size, price FROM brand RIGHT JOIN product ON brand.id = product.brandId WHERE brand.id = ? ORDER BY id DESC";
         db.query(getProducts, [brandId], async (err, data) => {
             if (err) {
                 return res.status(401).json({ error: "Something went wrong" });
             }
-            res.status(200).json({ products: data });
+            const products = data.map((product) => {
+                return {
+                    ...product,
+                    color: JSON.parse(product.color),
+                    size: JSON.parse(product.size),
+                };
+            });
+            res.status(200).json({ products: products });
         });
     } catch (err) {
         res.status(500).json({ error: "Something went wrong" });
