cdf97ff257d22779d46bdc64a4e54d2484d9cd67
Shubham Mondal <shubhamank002@gmail.com>
2022-12-26 20:32:27 +0530

    Add favourite routes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -19,4 +19,21 @@ const userInfo = (req, res) => {
     }
 };
 
+const updateFavourites = async (req, res) => {
+    try {
+        const id = req.user.id;
+        const { fav } = req.body;
+        const updateFavourites = "UPDATE user SET favourites = ? WHERE id = ?";
+        db.query(updateFavourites, [fav, id], (err, data) => {
+            if (err) {
+                return res.status(401).json({ error: "Something went wrong" });
+            }
+            res.status(200).json({ msg: "Updated favourites" });
+        });
+    } catch (err) {
+        res.status(500).json({ error: "Something went wrong" });
+    }
+};
+
 module.exports.userInfo = userInfo;
+module.exports.updateFavourites = updateFavourites;
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { userInfo } = require("../controllers/user.controller");
+const { userInfo, updateFavourites } = require("../controllers/user.controller");
 const authenticateToken = require("../middleware/verifyJWT.middleware");
 
 router.get("/info", authenticateToken, userInfo);
+router.put("/favourites", authenticateToken, updateFavourites);
 
 module.exports = router;
